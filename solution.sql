5cf58cb174f9c25cd5ce33a4a575b819e1f62deb
Paritosh Prasad <128465174+paritoshCog@users.noreply.github.com>
2023-08-10 15:16:30 +0530

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,1 +1,16 @@
+-- ques 1
+select e.name, st.salary from employee e join salary_table st on st.id = e.id
+where st.salary < (select max(salary) from salary_table)
+order by st.salary desc
+fetch first 1 rows only;
+
+-- or
+
+with abc as (SELECT name,dense_rank() over(ORDER by salary desc) as rank FROM employee e JOIN salary_table s ON e.id=s.id
+)
+SELECT name FROM abc where rank=2;
+
+
+
+
 
